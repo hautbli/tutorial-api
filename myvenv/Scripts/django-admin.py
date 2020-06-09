468d34ae84cfef8a32643d67d2cd3bb7c67a3306
withHappy <hap2.dev@gmail.com>
2020-06-09 12:02:13 +0900

    ignore folder

--- myvenv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\zeny\pycharmprojects\repo\tutorial-api\myvenv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
