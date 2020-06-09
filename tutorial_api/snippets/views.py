5b0b76ff28706e462d52d5527c756b716038d82d
withHappy <hap2.dev@gmail.com>
2020-06-09 12:37:13 +0900

    Revert "Revert "refactor : update viewset , router""
    This reverts commit 65de3cdf

--- tutorial_api/snippets/views.py
+++ tutorial_api/snippets/views.py
@@ -1,10 +1,22 @@
 from rest_framework.decorators import action
+from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 from rest_framework import viewsets
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 
 
-class SnippetViewSet(viewsets.ModelViewSet):
-    queryset = Snippet.objects.all()
-    serializer_class = SnippetSerializer
+class SnippetViewSet(viewsets.ViewSet):
+    """
+    A simple ViewSet for listing or retrieving users.
+    """
+    def list(self, request):
+        queryset = Snippet.objects.all()
+        serializer = SnippetSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        queryset = Snippet.objects.all()
+        user = get_object_or_404(queryset, pk=pk)
+        serializer = SnippetSerializer(user)
+        return Response(serializer.data)
